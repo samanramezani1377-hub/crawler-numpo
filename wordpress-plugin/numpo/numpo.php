4a5399b7dbed83f033e77b629963eb378810e9c6
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-24 17:02:39 +0330

    Bump Numpo plugin version for bundled runtime

--- wordpress-plugin/numpo/numpo.php
+++ wordpress-plugin/numpo/numpo.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: Numpo
  * Description: Numpo crawler control plane for WordPress.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Requires PHP: 7.4
  */
 if (!defined('ABSPATH')) exit;
-define('NUMPO_VERSION','0.1.0');
+define('NUMPO_VERSION','0.2.0');
 define('NUMPO_DIR',plugin_dir_path(__FILE__));
 define('NUMPO_MAIN_FILE',__FILE__);
 require_once NUMPO_DIR.'includes/class-numpo-settings.php';
