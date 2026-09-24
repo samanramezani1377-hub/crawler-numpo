4d18130a637cba9c9b6ec545b543e7dcaa220b36
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-24 11:42:25 +0330

    feat: add configurable autonomous search provider endpoint

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,6 @@ package config
 
 import "os"
 
-type Config struct { ListenAddr string; DatabaseURL string; APIKey string; HTTPTimeoutSeconds int; MaxBodyBytes int64 }
-func Load() Config { return Config{ListenAddr: env("NUMPO_LISTEN_ADDR", ":8080"), DatabaseURL: env("NUMPO_DATABASE_URL", "postgres://postgres:postgres@localhost:5432/numpo?sslmode=disable"), APIKey: os.Getenv("NUMPO_API_KEY"), HTTPTimeoutSeconds: 15, MaxBodyBytes: 2 << 20} }
+type Config struct { ListenAddr string; DatabaseURL string; APIKey string; SearchURLTemplate string; HTTPTimeoutSeconds int; MaxBodyBytes int64 }
+func Load() Config { return Config{ListenAddr: env("NUMPO_LISTEN_ADDR", ":8080"), DatabaseURL: env("NUMPO_DATABASE_URL", "postgres://postgres:postgres@localhost:5432/numpo?sslmode=disable"), APIKey: os.Getenv("NUMPO_API_KEY"), SearchURLTemplate: os.Getenv("NUMPO_SEARCH_URL_TEMPLATE"), HTTPTimeoutSeconds: 15, MaxBodyBytes: 2 << 20} }
 func env(k,d string) string { if v:=os.Getenv(k); v!="" { return v }; return d }
