97b8eadb16bfe7b6e541966c99af6ec19d123983
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-24 13:17:38 +0330

    test CGNAT SSRF blocking

--- internal/urlnorm/url_test.go
+++ internal/urlnorm/url_test.go
@@ -2,3 +2,5 @@ package urlnorm
 import "testing"
 func TestURLNormalization(t *testing.T){u,e:=URL("HTTP://Example.COM/a#frag");if e!=nil{t.Fatal(e)};if u.String()!="http://example.com/a"{t.Fatalf("got %s",u.String())}}
 func TestDomain(t *testing.T){d,e:=Domain("https://Shop.Example.com/x");if e!=nil{t.Fatal(e)};if d!="shop.example.com"{t.Fatalf("got %s",d)}}
+
+func TestCGNATPrivate(t *testing.T){if !IsPrivateHost("100.64.1.1"){t.Fatal("expected CGNAT block")}}
