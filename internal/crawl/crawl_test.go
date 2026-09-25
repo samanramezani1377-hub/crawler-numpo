b2b7451bb792802ff5300652663144415f7d7e13
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-25 17:23:59 +0330

    Remove obsolete Go runtime from PHP-only branch

--- internal/crawl/crawl_test.go
+++
@@ -1,3 +0,0 @@
-package crawl
-import("net/url";"testing")
-func TestParseHTML(t *testing.T){u,_:=url.Parse("https://example.com/a/");title,links:=ParseHTML(u,"<html><title>Test</title><a href='/x'>x</a><a href='https://example.com/y#f'>y</a></html>",10);if title!="Test"{t.Fatal(title)};if len(links)!=2{t.Fatalf("%v",links)}}
