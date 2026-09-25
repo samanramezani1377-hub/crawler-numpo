80485f09098a5c68bc6beeaec77d10f76733d30d
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-25 18:11:55 +0330

    Recover interrupted crawl jobs and stale candidates

--- wordpress-plugin/numpo/includes/class-numpo-worker.php
+++ wordpress-plugin/numpo/includes/class-numpo-worker.php
@@ -4,7 +4,7 @@ class Numpo_Worker {
  public static function init(){add_action('numpo_process_job',[__CLASS__,'process']);add_action('numpo_recover_jobs',[__CLASS__,'recover']);if(!wp_next_scheduled('numpo_recover_jobs'))wp_schedule_event(time()+300,'hourly','numpo_recover_jobs');}
  public static function schedule($job){if(!wp_next_scheduled('numpo_process_job',[$job]))wp_schedule_single_event(time()+1,'numpo_process_job',[$job]);}
  public static function recover(){
-  global $wpdb;$t=Numpo_DB::tables();$jobs=$wpdb->get_col("SELECT id FROM {$t['jobs']} WHERE status='running' AND updated_at<DATE_SUB(UTC_TIMESTAMP(),INTERVAL 15 MINUTE) LIMIT 20");foreach($jobs as $job)self::schedule($job);
+  global $wpdb;$t=Numpo_DB::tables();$jobs=$wpdb->get_col("SELECT id FROM {$t['jobs']} WHERE status IN ('running','queued') AND updated_at<DATE_SUB(UTC_TIMESTAMP(),INTERVAL 15 MINUTE) LIMIT 20");foreach($jobs as $job){Numpo_DB::recover_stale_candidates($job,15);self::schedule($job);}
  }
  public static function process($job){
   $row=Numpo_DB::get_job($job);if(!$row)return;
