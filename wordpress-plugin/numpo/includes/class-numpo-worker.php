5a2bcfc9b0bfd92c96e84f5ac1d880d932608fda
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-25 17:18:07 +0330

    Fix PHP-only source line endings

--- wordpress-plugin/numpo/includes/class-numpo-worker.php
+++ wordpress-plugin/numpo/includes/class-numpo-worker.php
@@ -18,8 +18,12 @@ class Numpo_Worker {
   if(is_wp_error($result)){Numpo_DB::retry_candidate($candidate,$result->get_error_message());self::schedule($job);return;}
   Numpo_DB::increment_job($job,'processed_urls');
   $domain_id=Numpo_DB::ensure_domain($row['project_id'],$candidate['normalized_domain']);
-  Numpo_DB::add_page($domain_id,$result['url'],$result['status'],$result['title'],$result['content_type'],$candidate['depth']);\n  foreach($result['headers']??[] as $h)Numpo_DB::add_fact($job,$domain_id,'http_header',$h[0],$h[1],$h[1],$result['url'],.7,'response-header');
-  foreach(Numpo_Crawler::technologies($result['body'],$result['url']) as $tech)Numpo_DB::add_technology($domain_id,$tech[0],$tech[1],[$tech[2]],$result['url']);\n  foreach(Numpo_Crawler::metadata($result['body']) as $meta)Numpo_DB::add_fact($job,$domain_id,'metadata',$meta[0],$meta[1],$meta[1],$result['url'],.8,'html-meta');\n  foreach(Numpo_Crawler::socials($result['body']) as $social)Numpo_DB::add_fact($job,$domain_id,'social',$social[0],$social[1],$social[1],$result['url'],.85,'social-link');\n  foreach(Numpo_Crawler::business($result['body']) as $biz)Numpo_DB::add_fact($job,$domain_id,'business',$biz[0],$biz[1],$biz[1],$result['url'],.75,'business-marker');
+  Numpo_DB::add_page($domain_id,$result['url'],$result['status'],$result['title'],$result['content_type'],$candidate['depth']);
+  foreach($result['headers']??[] as $h)Numpo_DB::add_fact($job,$domain_id,'http_header',$h[0],$h[1],$h[1],$result['url'],.7,'response-header');
+  foreach(Numpo_Crawler::technologies($result['body'],$result['url']) as $tech)Numpo_DB::add_technology($domain_id,$tech[0],$tech[1],[$tech[2]],$result['url']);
+  foreach(Numpo_Crawler::metadata($result['body']) as $meta)Numpo_DB::add_fact($job,$domain_id,'metadata',$meta[0],$meta[1],$meta[1],$result['url'],.8,'html-meta');
+  foreach(Numpo_Crawler::socials($result['body']) as $social)Numpo_DB::add_fact($job,$domain_id,'social',$social[0],$social[1],$social[1],$result['url'],.85,'social-link');
+  foreach(Numpo_Crawler::business($result['body']) as $biz)Numpo_DB::add_fact($job,$domain_id,'business',$biz[0],$biz[1],$biz[1],$result['url'],.75,'business-marker');
   Numpo_Discovery::seed_links($job,$row['project_id'],$result['url'],$result['body'],$result['url']);
   foreach(Numpo_Crawler::classify($result['body'],$result['url']) as $cls)Numpo_DB::add_fact($job,$domain_id,'page_classification',$cls[0],$cls[0],$cls[0],$result['url'],$cls[1],$cls[2]);
   foreach(Numpo_Crawler::contacts($result['body']) as $contact)Numpo_DB::add_contact($domain_id,$contact[0],$contact[1],$contact[2],$result['url']);
